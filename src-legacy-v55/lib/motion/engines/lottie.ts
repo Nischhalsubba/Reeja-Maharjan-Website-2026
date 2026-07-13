1f34ee3b6ba1b4b241b9e00c0fd3d3d2b3205846
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-07-13 15:04:29 +0545

    chore: remove retired source generations

--- src-legacy-v55/lib/motion/engines/lottie.ts
+++
@@ -1,7 +0,0 @@
-import { loadCached } from '../core/loader';
-
-export const loadLottie = async () =>
-  loadCached('engine:lottie', async () => {
-    const mod = await import('lottie-web');
-    return mod.default;
-  });
